9147cb6dacffc1474c4c6d2fa8c87b35584b5d9c
0xsolid22 <smart.solidity@gmail.com>
2023-03-09 18:35:42 -0500

    fix: dotenv added

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,6 +1,8 @@
 import { HardhatUserConfig } from 'hardhat/config';
 import '@nomicfoundation/hardhat-toolbox';
 
+require('dotenv').config();
+
 const GOERLI_URI = process.env.GOERLI_URI;
 const PRIVATE_KEY = process.env.PRIVATE_KEY || '';
 
