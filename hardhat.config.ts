42b334bd27ab6f5e12dd93ec1054bb8f2d8c1204
0xsolid22 <smart.solidity@gmail.com>
2023-03-09 18:50:52 -0500

    feat: verify on Etherscan

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -5,6 +5,7 @@ require('dotenv').config();
 
 const GOERLI_URI = process.env.GOERLI_URI;
 const PRIVATE_KEY = process.env.PRIVATE_KEY || '';
+const API_KEY = process.env.ETHERSCAN_API_KEY || '';
 
 const config: HardhatUserConfig = {
   solidity: '0.8.9',
@@ -12,6 +13,11 @@ const config: HardhatUserConfig = {
     goerli: {
       url: GOERLI_URI,
       accounts: [PRIVATE_KEY],
+    },
+  },
+  etherscan: {
+    apiKey: {
+      goerli: API_KEY,
     },
   },
 };
