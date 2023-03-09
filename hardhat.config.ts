4b4807df8cc47ff3d1f1560a54a1106ede8cadda
0xsolid22 <smart.solidity@gmail.com>
2023-03-09 12:27:20 -0500

    feat: add goerli network

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,17 @@
-import { HardhatUserConfig } from "hardhat/config";
-import "@nomicfoundation/hardhat-toolbox";
+import { HardhatUserConfig } from 'hardhat/config';
+import '@nomicfoundation/hardhat-toolbox';
+
+const GOERLI_URI = process.env.GOERLI_URI;
+const PRIVATE_KEY = process.env.PRIVATE_KEY || '';
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.9",
+  solidity: '0.8.9',
+  networks: {
+    goerli: {
+      url: GOERLI_URI,
+      accounts: [PRIVATE_KEY],
+    },
+  },
 };
 
 export default config;
